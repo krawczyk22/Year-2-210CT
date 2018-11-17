d1434f1df67cd12a6bb4cb2e2db848dbb6ced5e0
Michal Krawczykowski (krawczy5) <krawczy5@coventry.ac.uk>
2018-11-17 00:07:16 +0000

    Update cw12.py

--- cw12.py
+++ cw12.py
@@ -1,11 +1,17 @@
-#defining the binary tree class
+'''The programme implements a Binary Search Tree that hoads English words.
+The programme reads words from a text file, determinates their frequencies,
+prints the tree in pre-order, deleted a node and finds words and prints the 
+traversed path as well as indicated if the word exists in the tree.
+The programme provides a menu for its users. The code has been created
+based on the code provided on the Coventry University Moodle website, and 
+changed respectively. The programmehas been created by implementing pseudocodes'''
+
 class BinTreeNode(object):
   def __init__(self, value):
       self.value=value
       self.left=None
       self.right=None
-
-#Inserting a node to the tree              
+              
 def tree_insert(tree, item):
   if tree==None:
       tree=BinTreeNode(item)
@@ -22,6 +28,7 @@ def tree_insert(tree, item):
               tree_insert(tree.right,item)
   return tree
 
+#Function that returns the words in a string and their occurrences
 def occurrences(words):
   frequencies = dict()
   for word in words:
@@ -31,7 +38,6 @@ def occurrences(words):
       frequencies[word] = 1
   return frequencies
   
-#Function that prints the tree in pre-order
 def pre_order(tree):
   print(tree.value)
   if(tree.left!=None):
@@ -39,29 +45,27 @@ def pre_order(tree):
   if(tree.right!=None):
       pre_order(tree.right)
         
-#Function showing the path traversed while searching a node and indicating if the node is found
+#Function tam returns the path traversed while searching for the word along with the information if the word is found
 def tree_find(tree, target):
   path = "The path traversed is: "
-  r = tree
-  while r != None:
-    if r.value == target:
-      path = path + str(r.value) + " "
+  tempTree = tree
+  while tempTree != None:
+    if tempTree.value == target:
+      path = path + str(tempTree.value) + " "
       return(path + "\nyes")
-    elif r.value > target:  
-      path = path + str(r.value) + " "
-      r = r.left
+    elif tempTree.value > target:  
+      path = path + str(tempTree.value) + " "
+      tempTree = tempTree.left
     else:
-      path = path + str(r.value) + " "
-      r = r.right
+      path = path + str(tempTree.value) + " "
+      tempTree = tempTree.right
   return(path + "\nno")
 
 def minValue(tree): 
-    node = tree
-    while(node.left is not None): 
-        node = node.left 
-    return node  
+    while(tree.left is not None): 
+        tree = tree.left 
+    return tree  
   
-#Function that deletes a node from the binary tree
 def delete_node(tree, value):
   if tree is None: 
         return tree
@@ -72,35 +76,35 @@ def delete_node(tree, value):
           tree.right = delete_node(tree.right, value) 
     else:
       if tree.left is None : 
-        temp = tree.right  
-        tree = None 
-        return temp  
+        temp = tree.right  #storing the right subtree in a temporary variable
+        tree = None    #deleting the current node
+        return temp    #returning the stored subtree
       elif tree.right is None : 
         temp = tree.left  
         tree = None
         return temp 
       else:
-        temp = minValue(tree.right) 
-        tree.value = temp.value
-        tree.right = delete_node(tree.right , temp.value) 
+        temp = minValue(tree.right)  #Finding the minimum value in the subtree
+        tree.value = temp.value    #Assigning the value to the current value in the tree
+        tree.right = delete_node(tree.right , temp.value)      #deleting minimum found value from the subtree
     return tree
-  
     
 if __name__ == '__main__':
   
   try:
     f = open("words.txt", "r")
-    string = f.read().split()  #opening the file, reading the string and splitting it into a list
+    string = f.read().split()  
     f.close()
     
-    t=tree_insert(None,string[0]);  #defining the tree (assigning the root)
-
+    t=tree_insert(None,string[0]);    #definig the root of the tree
     for word in range(1,len(string)):
-      tree_insert(t,string[word])   #iterating through the list of words and assigning them to the list
+      tree_insert(t,string[word])   
           
-  except IndexError: print("The file does not have any values")   #throwing an error while the text file is empty
-  
+  except IndexError: print("The file does not have any values")   
+    
   basecase=0
+  
+  #Implementation of the menu
   while basecase == 0:
     print("What do you want to do?")
     print("1. Print the list of words with their occurrences")
@@ -117,16 +121,21 @@ if __name__ == '__main__':
 
     if answer == 1:
       print(str(occurrences(string)))
+      
     elif answer == 2:
       pre_order(t)
+      
     elif answer == 3:
       answerFind=str(input("What is the word you want to find? \n"))
       print(tree_find(t, answerFind))
+      
     elif answer == 4:
       answerDelete=str(input("What is the word you want to delete? \n"))
       if answerDelete not in string:
         print("There is no such word in the tree")
       else: t=delete_node(t, answerDelete)
+        
     elif answer == 5:
       basecase=1
+      
     else: print("There is no such option")
